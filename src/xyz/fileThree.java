d66fb4dc0324761fda4e7709e65c2ec63b4a26a7
edwinkip <edwinkiplagat40@gmail.com>
2019-02-25 16:54:11 +0300

    first commit

---
+++ src/xyz/fileThree.java
@@ -0,0 +1,19 @@
+package xyz;
+
+public class fileThree {
+    public static void main(String[] args) {
+        String[] wanafunzi;
+        String output;
+        wanafunzi = new String[5];
+        wanafunzi[0] = "Anthony";
+        wanafunzi[1] = "Ian";
+        wanafunzi[2] = "Peter";
+        wanafunzi[3] = "Brio";
+        wanafunzi[4] = "wasike";
+        output = wanafunzi[0]+"\n"+wanafunzi[1]+"\n"+
+                wanafunzi[2]+"\n"+wanafunzi[3]+"\n"+
+                wanafunzi[4];
+        System.out.println(output);
+
+    }
+}
